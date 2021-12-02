ab6187d97746ff5a370ed6f26535740b8d3ea17d
Guinevere Remaut <guinevere.remaut@hotmail.be>
2021-12-02 20:23:55 +0100

    Updated the background

--- utils/storyblok.js
+++ utils/storyblok.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import StoryblokClient from "storyblok-js-client";
 
 const Storyblok = new StoryblokClient({
-  accessToken: "c1g4yy5Pro9MeJrxV9grQAtt",
+  accessToken: "6IggLP91c8pf5EKVmyUxnwtt",
   cache: {
     clear: "auto",
     type: "memory",
